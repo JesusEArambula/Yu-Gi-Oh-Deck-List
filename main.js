a07e45b808ce45e5a1416db23454a056b744519e
JesusEArambula <61527697+JesusEArambula@users.noreply.github.com>
2020-04-10 00:25:41 -0700

    First Commit

---
+++ main.js
@@ -0,0 +1,57 @@
+function getParameterByName(name, url)
+{
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return "";
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var getAllRecords = function() {
+    $.getJSON(
+      "https://api.airtable.com/v0/appTp6UQvc8Q7F18e/Decks?api_key=keyorwAJW52Aa0nI4",
+      function(airtable) {
+        var html = [];
+        $.each(airtable.records, function(index, record) {
+          var id = record.id;
+          // Play with this section only --------------------------------
+          var picture = record.fields["Picture"];
+          var deckName = record.fields["Deck Name"];
+          var strategy = record.fields["Strategy"];
+        
+          // Replace variable names to push
+          html.push(listView(id, picture, deckName, strategy));
+          // playing ends here ------------------------------------------
+        });
+        $(".list-view").append(html);
+      }
+    );
+  };
+
+
+var listView = function(id, picture, deckName, strategy) {
+    return `
+      <div class="card border-dark">
+        ${picture ? `<img src="${picture[0].url}" class="card-img-top" style="background-color: navy">` : ``}
+        
+        <div class="name">
+          <h5>${deckName}</h5>
+        </div>
+      </div>
+  
+
+    `;
+  };
+
+
+var id = getParameterByName("id");
+if (id)
+{
+  getOneRecord(id);
+} 
+else
+{
+  getAllRecords();
+}
